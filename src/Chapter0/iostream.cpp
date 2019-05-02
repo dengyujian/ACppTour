13d8c43716b9805cdeb2154a99eac2ac78c7643a
ShannonChen <shannonchenchn@foxmail.com>
2019-05-02 13:55:38 +0800

    添加第 0 章的练习代码

---
+++ src/Chapter0/iostream.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	std::cout << "Enter two numbers:" << std::endl;
+	/*
+	上面一行等价于下面这两行
+	std::cout << "Enter two numbers:";
+	std::cout << std::endl;
+	*/
+	int v1 = 0, v2 = 0;
+	std::cin >> v1 >> v2;
+	std::cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+
+
+	int sum = 0, value = 0;
+	while (std::cin >> value) { // 不停接收输入的数字，直到按下 ctrl+D 才停止循环
+		sum += value;
+	}
+	std::cout << "Sum is " << sum << std::endl;
+
+	return 0;
+}
